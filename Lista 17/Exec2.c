a1aeb6c9fe43e0bc12ed0ef5160b08e4845213ee
Luca C. Libório <lucaliborio33@gmail.com>
2023-05-29 17:06:17 -0300

    Update

--- Lista 17/Exec2.c
+++ Lista 17/Exec2.c
@@ -12,20 +12,46 @@
    
   void main(){
   	
-  	int conta, cont;
-  	float saldo;
+  	int conta, qtdetotal = 1, saldneg = 0;
+  	float saldo = 0, saldototal = 0;
   	char nome[100];
   	
   	do{
   		printf("Informe o n° da conta: ");
   		scanf("%d", &conta);
-  		printf("Informe o nome: ");
-		scanf("%s", nome);
-  		printf("Informe o saldo da conta: ");
-  		scanf("%f", &saldo);
+  		if(conta != -999){
+  			printf("Informe o nome: ");
+			scanf("%s", nome);
+  			printf("Informe o saldo da conta: ");
+  			scanf("%f", &saldo);
+		  }
   		
-  		
-  		
-  		cont++;
-	  }	while(conta = -999 && cont <= 10 );
-  }
+  		if(saldo < 0){
+  			printf("Conta: %d\n", conta);
+  			printf("Saldo: %0.2f\n", saldo);
+  			printf("Saldo Negativo!!!\n");
+  			
+  			saldneg++;
+		  }
+		  else
+		  	if(saldo > 0){
+		  	printf("Conta: %d\n", conta);
+  			printf("Saldo: %0.2f\n", saldo);
+  			printf("Saldo Positivo!!!\n");
+  			
+		  }
+		  else{
+		  	printf("Sem saldo!!!\n");
+		  }
+			
+			qtdetotal++;
+			saldototal = saldo + saldototal;
+	  }while(qtdetotal <=10);
+	  
+	  
+	  printf("Qtde de pessoas com saldo negativo: %d\n", saldneg);
+	  printf("Qtde total de clientes da agencia: %d\n", qtdetotal-1);
+	  printf("Saldo total de todos os clientes: %0.2f\n", saldototal);
+}
+	  
+  
