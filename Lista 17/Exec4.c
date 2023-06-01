28ecab8bbab4072b9438c3f1df70ce3910d93aa2
Luca C. Libório <lucaliborio33@gmail.com>
2023-06-01 15:11:44 -0300

    Create Exec4.c

---
+++ Lista 17/Exec4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <conio.h>
+
+/*4 - Escreva um algoritmo que receba 132 números, calcule e exiba as quantidades
+	 de números pares e de números ímpares existentes neste conjunto conhecido.*/
+   
+   
+  void main(){
+  	
+  }
