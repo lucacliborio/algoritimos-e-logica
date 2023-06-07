df357590a5ced8d73461a38eeca17b7f8b401850
Luca C. Libório <lucaliborio33@gmail.com>
2023-06-06 21:07:27 -0300

    Update Exec3
    Finalizando o exec3

--- Lista 19/Exec3.c
+++ Lista 19/Exec3.c
@@ -15,13 +15,13 @@
 	"	A maior e menor nota da turma; --
 	"	A nota média da turma; ok
 	"	O total de alunos reprovados; ok
-	"	A porcentagem de alunos reprovados por faltas. --
+	"	A porcentagem de alunos reprovados por faltas. ok
 */
 
 void main(){
 	
-	int i = 1, matricula = 0, aula = 0, reprovados = 0, reprov1 = 0, reprov2 = 0;
-	float n1 = 0, n2 = 0, n3 = 0, med = 0, reprovadosfalta = 0, totaln = 0, medtotal = 0, nmaior;
+	float i = 1, n1 = 0, n2 = 0, n3 = 0, med = 0 , reprovadosfalta = 0 , totaln = 0, medtotal = 0 ;
+	float  nmaior = 0, nmenor = 99999, somamed = 0, reprovados = 0, reprov1 = 0, reprov2 = 0, matricula = 0, aula = 0;
 	
 	do{
 		printf("Informe a sua mahtricular: ");
@@ -33,9 +33,8 @@ void main(){
 		printf("Informe a sua terceira nota: ");
 		scanf("%f", &n3);
 		
-		totaln = n1+n2+n3;//totalnotas
-		medtotal = (totaln + medtotal)/i;//media total
-		med = (n1+n2+n3) /3;//conta media
+		med = (n1+n2+n3) /3;//conta media dos alunos
+		somamed = (med + somamed);//media total
 		
 		if(med >= 6){//aprovados
 			printf("Informe em quantas aulas voce compareceu: ");
@@ -47,38 +46,55 @@ void main(){
 			else{//reprovados por falta
 				printf("Reprovado por falta!!!\n");
 				printf("Sua mehdia foi: %0.1f\n", med);
-				reprov1 ++;
+				reprov1++;
 			}
 		}
 		else{// reprovados por nota
 			printf("Reprovado!!!\n");
 			printf("Sua mehdia foi: %0.1f\n", med);
-			reprov2 ++;
+			reprov2++;
 		}
 		
 		i++;
 		
+		//atribuindo a maior nota
 		if(n1 > nmaior){
 			nmaior = n1;
 		}
-		else
-			if(n2 > nmaior){
-				nmaior = n2;
-			}
-			else
-				if(n3 > nmaior){
-					nmaior = n3;
-				}
 		
-	}while(i <=1);
+		if(n2 > nmaior){
+			nmaior = n2;
+		}
 	
+		if(n3 > nmaior){
+			nmaior = n3;
+		}
+		
+		//atribuindo a menor nota
+		if(nmenor > n1){
+			nmenor = n1;
+		}
+		
+		if(nmenor > n2){
+			nmenor = n2;
+		}
+	
+		if(nmenor > n3){
+			nmenor = n3;
+		}
+		
+	}while(i <=2);
+	
+	//media da turma
+	medtotal = somamed/(i - 1);
 	//conta total rerovados
 	reprovados = reprov1 + reprov2;
 	//conta % reprovados por falta
 	reprovadosfalta =  (reprov1/reprovados)*100;
 	
 	printf("A maior nota foi: %0.1f\n", nmaior);
+	printf("A menor nota foi: %0.1f\n", nmenor);
 	printf("Media da turma: %0.1f\n", medtotal);
-	printf("Alunos reprovados: %d\n", reprovados);
+	printf("Alunos reprovados: %0.0f\n", reprovados);
 	printf("Alunos reprovados por falta: %0.1f%%", reprovadosfalta);
 }
